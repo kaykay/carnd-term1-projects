3c398361e3a7296167b36b77390d9d7a8840416e
kaykay <kitti.k@gmail.com>
2017-06-27 00:41:25 -0700

    forgot to commit this with previous update

--- lane_finding/finding_lines.py
+++ lane_finding/finding_lines.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
@@ -75,6 +76,9 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 Prev_result = [None]
 
+import queue
+prev_poly = queue.Queue(4)
+
 def process_img(image):
     undistort = cal_undistort(image, mtx, dist)
     #warp
@@ -137,26 +141,45 @@ def process_img(image):
         leftx[level*window_height:(level + 1) * window_height] = window_centroids[level][0]
         rightx[level*window_height:(level + 1) * window_height] = window_centroids[level][1]
 
+    lane_center = leftx[0] + (rightx[0] - leftx[0]) / 2
+    img_center = 1280/2
+
+    bottom_dist = rightx[0] - leftx[0]
+    top_dist = rightx[-1] - leftx[-1]
+    
+    #number of pixels off center
+    off_center_in_pixels = img_center - lane_center
+    
     leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
     rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
+    
     xm_per_pix = 3.7/700
+    off_center_in_meters = off_center_in_pixels * xm_per_pix
+    bottom_dist_metres = bottom_dist *  xm_per_pix
+    top_dist_metres = top_dist * xm_per_pix
+    
     avg_dist = np.mean(rightx - leftx) * xm_per_pix
     
     print("Average distance in meters: ", np.mean(rightx - leftx) * xm_per_pix)
     left_fit = np.polyfit(ploty, leftx, 2)
-    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
     right_fit = np.polyfit(ploty, rightx, 2)
+
+    if (Prev_result[0] != None and (bottom_dist_metres < 3.85 or bottom_dist_metres > 4.15)):
+        left_fit, right_fit = Prev_result[0]
+    else:
+        Prev_result[0] = (left_fit, right_fit)
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
     right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
 
-#    mark_size = 3
-#    plt.plot(leftx, ploty, 'o', color='red', markersize=mark_size)
-#    plt.plot(rightx, ploty, 'o', color='blue', markersize=mark_size)
-#    plt.xlim(0, 1280)
-#    plt.ylim(0, 720)
-#    plt.plot(left_fitx, ploty, color='green', linewidth=3)
-#    plt.plot(right_fitx, ploty, color='green', linewidth=3)
-#    plt.gca().invert_yaxis()
-#    plt.show()
+#   mark_size = 3
+#   plt.plot(leftx, ploty, 'o', color='red', markersize=mark_size)
+#   plt.plot(rightx, ploty, 'o', color='blue', markersize=mark_size)
+#   plt.xlim(0, 1280)
+#   plt.ylim(0, 720)
+#   plt.plot(left_fitx, ploty, color='green', linewidth=3)
+#   plt.plot(right_fitx, ploty, color='green', linewidth=3)
+#   plt.gca().invert_yaxis()
+#   plt.show()
     
     y_eval = np.max(ploty)
     left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
@@ -175,12 +198,12 @@ def process_img(image):
     right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
     # Now our radius of curvature is in meters
     print(left_curverad, 'm', right_curverad, 'm')
-
+    
     #Draw image
     # Create an image to draw the lines on
     warp_zero = np.zeros_like(warped).astype(np.uint8)
     color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
-    
+
     # Recast the x and y points into usable format for cv2.fillPoly()
     pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
     pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
@@ -188,17 +211,24 @@ def process_img(image):
     
     # Draw the lane onto the warped blank image
     cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
-    if (avg_dist < 3.5 or avg_dist > 4.5):
-        return Prev_result[0] #Returns previous result
     # Warp the blank back to original image space using inverse perspective matrix (Minv)
     newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0])) 
     # Combine the result with the original image
     result = cv2.addWeighted(undistort, 1, newwarp, 0.3, 0)
-    result = cv2.putText(result,'Avg dist: ' + str(avg_dist), (500,30), font, 1,(255,255,255),2)
-    result = cv2.putText(result,'Left curvature: ' + str(left_curverad), (500,60), font, 1,(255,255,255),2)
+    if (off_center_in_meters > 0):
+        result = cv2.putText(result,'Vehicle is: ' + str(off_center_in_meters) +  ' left of center', (500,30), font, 1,(255,255,255),2)
+    else:
+        result = cv2.putText(result,'Vehicle is: ' + str(abs(off_center_in_meters)) +  ' right of center', (500,30), font, 1,(255,255,255),2)
+        
+    result = cv2.putText(result,'left curvature: ' + str(left_curverad ), (500,60), font, 1,(255,255,255),2)
     result = cv2.putText(result,'Right curvature: ' + str(right_curverad), (500,90), font, 1,(255,255,255),2)
-    Prev_result[0] = result
+#    result = cv2.putText(result,'bottom dist: ' + str(bottom_dist_metres), (500,120), font, 1,(255,255,255),2)
+#    result = cv2.putText(result,'bottom dist: ' + str(top_dist_metres), (500,150), font, 1,(255,255,255),2)
+
     return result;
+
+
+
 
 result = process_img(image)
 plt.imshow(result)
